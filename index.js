735e9ec2b0dcae7a9f7c01238b48d0f5a9150912
SreevidyaNattuva <75021824+SreevidyaNattuva@users.noreply.github.com>
2021-12-02 12:05:21 +0530

    FinalCommitCashRegister
    FinalCommitCashRegister

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-givem");
+const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error-message");
-console.log(checkButton);
-
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+var notesArray = [2000, 500, 100, 20, 10,5,1];
 checkButton.addEventListener("click", function validateAmount(){
+    console.log(isNaN(billAmount.value));
     errorMessage.style.display = "none";
-    if(billAmount.value>0){
+    if((billAmount.value>0) && !(isNaN(billAmount.value))){
        if(cashGiven.value>0){
            if(cashGiven.value>= billAmount.value){
-             const amountToBeReturned = cashGiven - billAmount;
+               
+             const returnAmount = cashGiven.value - billAmount.value;
+             console.log(returnAmount);
+             calculateNoOfNotes(returnAmount);
            }
            else{
             showErrorMessage("Cash given must be equal to bill amount");
@@ -17,11 +21,20 @@ checkButton.addEventListener("click", function validateAmount(){
        }
     }
     else{
-        showErrorMessage("bill amount is invalid");
+        showErrorMessage("Please enter a valid bill amount and bill amount must be a number");
+        }
+        function calculateNoOfNotes(returnAmount){
+            for(var i=0; i<notesArray.length; i++){
+                //console.log(returnAmount);
+                 var noOfNotestobeReturned = Math.trunc(returnAmount/notesArray[i]);
+                 returnAmount = returnAmount%notesArray[i];
+                 //console.log(returnAmount,noOfNotestobeReturned);
+                 noOfNotes[i].innerText = noOfNotestobeReturned;
+            }
         }
 });
-function showErrorMessage(errorMessage){
+function showErrorMessage(message){
     errorMessage.style.display = "block";
-    errorMessage.innerText = errorMessage;
+    errorMessage.innerText = message;
 
 }
